58d35f12bc2872e8dbf168831cf0008af88c0abb
atik94 <atikurrahmanatik94@gmail.com>
2022-10-26 13:13:03 +0600

    courses api create

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("Course api running");
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
